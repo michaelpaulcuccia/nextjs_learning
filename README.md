52dc5ef22c62fad644f53fbceaec51fdc06b76de
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-02-18 15:11:19 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nextjs_learning
+
+> SERVER-SIDE RENDERING: first page load to be rendered by server. unlike react, where the entire application is loaded and rendered on client.
---
+++ nextie/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <h3>
+        Hello, from index.js
+      </h3>
+    </div>
+  )
+}
