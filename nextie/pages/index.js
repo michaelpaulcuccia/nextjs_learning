0eaa70bc99f07a141481de122e984b40df2791d2
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-02-19 10:20:41 -0500

    getStaticProps()

--- nextie/pages/index.js
+++ nextie/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
-export default function Home() {
+export default function Home({ articles }) {
+
   return (
     <div>
       <h3>
@@ -9,6 +10,19 @@ export default function Home() {
         </Head>
         Hello, from index.js
       </h3>
+      {articles.map(article => <h4 key={article.id}>{article.title}</h4>)}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles
+    }
+  }
+
 }
